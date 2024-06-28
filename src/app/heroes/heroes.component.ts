6f0fd4d38d8d2bb717091dd1998a7a178773037a
Oksana Miazina <oksana.myazina@gmail.com>
2024-06-28 17:28:13 +0300

    add hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -13,8 +13,19 @@ import { Hero } from '../hero';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes?: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
