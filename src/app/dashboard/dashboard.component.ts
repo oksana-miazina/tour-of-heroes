d919eddb93c6e192113d4a579214ab6ce29f85f1
Oksana Miazina <oksana.myazina@gmail.com>
2024-06-28 18:17:31 +0300

    add dasboard component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
